337e6f81656043904db640932b182e9655f026fd
Michał Wierzbiński <mwe@spyro-soft.com>
2021-05-05 14:51:18 +0200

    Update climate sr page, Update image enhancement page

--- app/pages/climate_sr.py
+++ app/pages/climate_sr.py
@@ -14,8 +14,8 @@ def climate_sr_page():
     )
     st.markdown(
         "In climatology and geospatial sciences **\"downscaling\"** means *'to increase spatial resolution'* e.g., "
-        "going from 1/2 deg. grid into 1/8 deg. grid (4x resolution increase). While in image "
-        "processing world we would simply say **\"upscaling\"**. It might be confusing at first..."
+        "going from 1/2° grid into 1/8° grid (4x resolution increase). While in image processing world we would "
+        "simply say **\"upscaling\"**. It might be confusing at first..."
     )
 
     # ______Problem definition______
@@ -31,8 +31,8 @@ def climate_sr_page():
     )
     st.markdown(
         "This Neural-Downscaling approach for downscaling precipitation and temperature data can be used to downscale "
-        "from 1/2 degree (50km) to 1/8 degrees (12.5km) over the entirety of the globe, thus greatly reduce "
-        "computational cost associated with generating CSMs."
+        "from 1/2° (50 km) to 1/8° (12.5 km) over the entirety of the globe, thus greatly reduce computational cost "
+        "associated with generating CSMs."
     )
 
     # ______Implementation details______
@@ -41,13 +41,29 @@ def climate_sr_page():
 
     # ______Dataset______
     st.markdown("### Dataset")
+    st.markdown("We used 2 datasets:")
+    st.markdown(
+        "* [WorldClim](https://www.worldclim.org/) - WorldClim is a database of high spatial resolution global weather "
+        "and climate data. These data can be used for mapping and spatial modeling. The data are provided for use in "
+        "research and related activities. Historical monthly weather data for 1960-2018. The variables available are "
+        "average minimum temperature (°C), average maximum temperature (°C) and total precipitation (mm). The spatial "
+        "resolution is 2.5 minutes (0.0417°, ~21 km2). The WorldClim also provides elevation dataset (meters above "
+        "mean sea level)."
+    )
+    st.markdown(
+        "* [CRU-TS](https://crudata.uea.ac.uk/cru/data/hrg/) - The CRU TS v 4.04 dataset was developed and has been "
+        "subsequently updated, improved and maintained with support from a number of funders, principally the UK's "
+        "Natural Environment Research Council (NERC) and the US Department of Energy. Coverage: the period of "
+        "1901-2019, of land areas (excluding Antarctica) at 0.5° resolution. Variables: average minimum temperature "
+        "(°C), average average temperature (°C), average maximum temperature (°C) and precipitation (mm)."
+    )
     st.markdown(
         "For example, in our application, we obtain climate data through the [WorldClim](https://www.worldclim.org/) "
-        "dataset at a 1km spatial resolution which aggregates station observations to a grid with physical "
-        "and topographical information. We then upscale the precipitation data to 1/8 degree (12.5 km) as our "
-        "high-resolution observations. Following, we upscale further to 1/2 degree corresponding to a low-resolution "
+        "dataset at a 1 km spatial resolution which aggregates station observations to a grid with physical "
+        "and topographical information. We then upscale the precipitation data to 1/8° (12.5 km) as our "
+        "high-resolution observations. Following, we upscale further to 1/2° corresponding to a low-resolution "
         "climate (the same resolution as CRU-TS dataset). The goal is then to learn a mapping between our "
-        "low-resolution and high-resolution datasets"
+        "low-resolution and high-resolution datasets."
     )
 
     # ______Network______
@@ -68,17 +84,60 @@ def climate_sr_page():
     )
     st.markdown("* $H$ - the tile height: in our case 128")
     st.markdown("* $W$ - the tile width: in our case 128")
-    st.markdown("TODO, datasets, variables, raster resizing, tile generation, normalization, augmentations,")
+    st.markdown(
+        "Prior to training, the data was pre-processed by resizing WorldClim dataset to target HR resolution of 1/8°. "
+        "After resizing tile generation was performed. Tiles of size 128 $\\times$ 128 were generated using `Rasterio`"
+        " and `Dask`. "
+    )
+    st.markdown(
+        "While training each tile is normalized to fit in range [0;1] using prior knowledge of min and max values from "
+        "the parent raster. LR images are generated from HR source images by resizing from 128 $\\times$ 128 "
+        "to 32 $\\times$ 32 and then agiain resizing to 128 $\\times$ 128 nearest-neighbour interpolation. "
+        "Random vertical, horizontal flips and 90 rotations are applied at training time. This makes the network to "
+        "become spatially ignorant to tile geo-location."
+    )
 
     # ______Training loop______
     st.markdown("### Training loop")
-    st.markdown("TODO")
+    st.markdown(
+        "When training tiles of particular variable are concatenated with elevation data for that extent. The network "
+        "is trained with standard training scheme with following configuration:"
+    )
+    st.markdown("* Loss function: L2 Loss")
+    st.markdown("* Batch size: 64")
+    st.markdown("* Max learning rate: 1e-4")
+    st.markdown("* Optimizer: Adam (with default parameters)")
+    st.markdown(
+        """* LR Scheduler: OneCycle with:
+    * `pct_start: 0.05`
+    * `div_factor: 2.0`
+    * `final_div_factor: 1e2`"""
+    )
+    st.markdown("* Downscaling factor: 4x")
+    st.markdown("* HR image size: 128x128")
+    st.markdown("* LR image size: 32x32")
+    st.markdown("* Train years: 1961-1999")
+    st.markdown("* Validation years: 2000-2005")
+    st.markdown("* Test years: 2006-2019")
+    st.markdown("* Main performance metric: **RMSE**")
+    st.markdown(
+        """* Helper metrics:
+    * **MAE**
+    * **MSE**
+    * **PSNR**
+    * **SSIM**
+    * **denormalized MAE**
+    * **denormalized MSE**
+    * **denormalized RMSE**
+    * **denormalized $R^2$**
+    """
+    )
 
     # ______Tools and libs______
     st.markdown("### Tools and libs")
     st.markdown(
         "* [Python](https://www.python.org/): Python is an interpreted high-level general-purpose programming "
-        "language. Currently the most popuar language for data science and geospatial big data processing enthusiasts "
+        "language. Currently the most popular language for data science and geospatial big data processing enthusiasts "
         "and researchers."
     )
     st.markdown(
@@ -106,15 +165,37 @@ def climate_sr_page():
         "Rasterio reads and writes these formats and provides a Python API based on Numpy N-dimensional arrays "
         "and GeoJSON."
     )
+    st.markdown(
+        "* [Dask](https://dask.org/): Dask is an open source library for parallel computing written in Python. "
+        "Originally developed by Matthew Rocklin, Dask is a community project maintained and sponsored by developers "
+        "and organizations."
+    )
 
     # ______Training visualizations______
-    st.header("Training visualization")
-    st.markdown("todo")
+    st.header("Training visualization and monitoring")
+    st.markdown(
+        "Below you can see training progress visualization. Please note on how with each epoch the results become "
+        "sharper and more visually pleasing."
+    )
     st.image("./assets/climate-sr/training-progress.gif")
-    st.markdown("todo")
+    st.markdown(
+        "For training monitoring a tool called Tensorboard was used. It comes configured out of the box with Lightning."
+    )
+    st.image("./assets/climate-sr/tensorboard.png")
+    st.image("./assets/climate-sr/tensorboard2.png")
 
     # ______Perceptual results______
     st.header("Perceptual results")
-    st.markdown("todo")
-    st.markdown("todo")
-    st.markdown("todo")
+    st.markdown("Below you can see the network inference results.")
+    col1, col2 = st.beta_columns(2)
+
+    with col1:
+        st.markdown("LR input raster from CRU-TS (prec, tmin, tmax): ")
+        st.image("./assets/climate-sr/world-clim-temperature.png")
+        st.image("./assets/climate-sr/world-clim-temperature.png")
+        st.image("./assets/climate-sr/world-clim-temperature.png")
+    with col2:
+        st.markdown("HR output raster from the network:")
+        st.image("./assets/climate-sr/world-clim-temperature.png")
+        st.image("./assets/climate-sr/world-clim-temperature.png")
+        st.image("./assets/climate-sr/world-clim-temperature.png")
--- app/pages/history.py
+++ app/pages/history.py
@@ -1,7 +1,76 @@
 # -*- coding: utf-8 -*-
 import streamlit as st
 from app.pages import consts
-
+from glob import glob
+import os
 
 def history_page():
     st.title(consts.HISTORY_PAGE_NAME)
+    st.markdown("Original Image")
+
+    original_files = sorted(glob("./assets/image-enhancement/original/*png"))
+    lr_files = sorted(glob("./assets/image-enhancement/lr/*png"))
+
+    for filter_name, description in [
+        (
+            "Nearest",
+            "One of the simpler ways of increasing image size is nearest-neighbor interpolation, replacing every pixel "
+            "with the nearest pixel in the output; for upscaling this means multiple pixels of the same color will be "
+            "present. This can preserve sharp details in pixel art, but also introduce jaggedness in previously smooth "
+            "images. 'Nearest' in nearest-neighbor doesn't have to be the mathematical nearest. One common "
+            "implementation is to always round towards zero. Rounding this way produces fewer artifacts and is faster "
+            "to calculate."
+        ),
+        (
+            "Bilinear",
+            "Bilinear interpolation works by interpolating pixel color values, introducing a continuous transition "
+            "into the output even where the original material has discrete transitions. Although this is desirable "
+            "for continuous-tone images, this algorithm reduces contrast (sharp edges) in a way that may be "
+            "undesirable for line art. Bicubic interpolation yields substantially better results, with an increase "
+            "in computational cost."
+        ),
+        (
+            "Bicubic",
+            "Bilinear interpolation works by interpolating pixel color values, introducing a continuous transition "
+            "into the output even where the original material has discrete transitions. Although this is desirable for "
+            "continuous-tone images, this algorithm reduces contrast (sharp edges) in a way that may be undesirable "
+            "for line art. Bicubic interpolation yields substantially better results, with an increase in "
+            "computational cost."
+        ),
+        (
+            "Box",
+            "One weakness of bilinear, bicubic and related algorithms is that they sample a specific number of pixels. "
+            "When down scaling below a certain threshold, such as more than twice for all bi-sampling algorithms, the "
+            "algorithms will sample non-adjacent pixels, which results in both losing data, and causes rough results. "
+            "The trivial solution to this issue is box sampling, which is to consider the target pixel a box on the "
+            "original image, and sample all pixels inside the box. This ensures that all input pixels contribute to "
+            "the output. The major weakness of this algorithm is that it is hard to optimize."
+        ),
+        (
+            "Lanczos",
+            "Sinc resampling in theory provides the best possible reconstruction for a perfectly bandlimited signal. "
+            "In practice, the assumptions behind sinc resampling are not completely met by real-world digital images. "
+            "Lanczos resampling, an approximation to the sinc method, yields better results. Bicubic interpolation can "
+            "be regarded as a computationally efficient approximation to Lanczos resampling."
+        ),
+    ]:
+        st.markdown(f"### {filter_name}")
+        st.markdown(description)
+
+        col1, col2, col3 = st.beta_columns(3)
+        with col1:
+            st.markdown("LR")
+        with col2:
+            st.markdown(filter_name)
+        with col3:
+            st.markdown("HR")
+
+        for fname in original_files:
+            basename = os.path.basename(fname)
+            col1, col2, col3 = st.beta_columns(3)
+            with col1:
+                st.image(f"./assets/image-enhancement/lr/LR-{basename}")
+            with col2:
+                st.image(f"./assets/image-enhancement/upscaled/{filter_name.upper()}-{basename}")
+            with col3:
+                st.image(f"./assets/image-enhancement/hr/HR-{basename}")
